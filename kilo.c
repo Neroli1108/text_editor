643e7cddd328d69a709df61c0f4bddf000d5f248
Neroli1108 <neroli1108@gmail.com>
2019-11-05 00:12:21 -0800

    using the ansi to clear the sscreen and reposition the cursor.

--- kilo.c
+++ kilo.c
@@ -17,6 +17,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
@@ -55,9 +57,18 @@ void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
+}
+
+/*** output ***/
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
 }
 
 /*** init ***/
@@ -67,6 +78,7 @@ int main() {
     enableRawMode();
 
     while (1){
+        editorRefreshScreen();
         editorProcessKeypress(); 
     };
 
